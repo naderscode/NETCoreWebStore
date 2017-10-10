c9f23297c6a94eea56503ab85c6a3d0eceffa04f
naderscode <naderscode@gmail.com>
2017-10-09 17:38:35 -0700

    Initial commit

---
+++ SpyStore.DAL/EF/StoreContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using SpyStore.Models.Entities;
+
+namespace SpyStore.DAL.EF
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext()
+        {
+        }
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        protected override void OnConfiguiring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(
+                    @"Server=(localdb)\mssqllocaldb;Database=SpyStore;Trusted_Connection=True;MultipleActiveResultSets=true;", options => options.EnableRetryOnFailure());
+            }
+        }
+    }
+}
